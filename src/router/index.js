d85d5b856c487f0762e9b5e9118cf9ec7e3f73fb
yoniyoniee <shy7235@naver.com>
2023-09-15 17:59:36 +0900

    router: children route 중첩 경로..(에러)

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
 import PartInfo from '../parts/PartInfo.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotArmss from '../parts/RobotArms.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+import RobotBases from '../parts/RobotBases.vue';
 
 export default createRouter({
   history: createWebHashHistory(), // 해시태그 라우팅 또는 HTML5 라우팅 사용 여부에 영향을 준다.
@@ -13,6 +18,32 @@ export default createRouter({
     path: '/build', // 해당 경로로 가서 component 표시
     name: 'Build', // route에 부여하는 이름. 코드의 다양한 위치에서 사용 가능
     component: RobotBuilder,
+  }, {
+    path: '/parts/browse',
+    name: 'BrowseParts',
+    components: BrowseParts,
+    children: [
+      {
+        name: 'BrowseHeads',
+        path: 'heads',
+        component: RobotHeads,
+      },
+      {
+        name: 'BrowseArms',
+        path: 'arms',
+        component: RobotArmss,
+      },
+      {
+        name: 'BrowseTorsos',
+        path: 'torsos',
+        component: RobotTorsos,
+      },
+      {
+        name: 'BrowseBases',
+        path: 'bases',
+        component: RobotBases,
+      },
+    ],
   }, {
     path: '/parts/:partType/:id',
     name: 'Parts',
