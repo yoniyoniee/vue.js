2533643753cb7c044c2ecc59fe8363bb6ce3745f
yoniyoniee <shy7235@naver.com>
2023-09-18 13:26:28 +0900

    modules

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -80,7 +80,7 @@ export default {
   mixins: [createdHookMixin],
   computed: {
     availableParts() {
-      return this.$store.state.parts;
+      return this.$store.state.robots.parts;
     },
     saleBorderClass() {
       return this.selectedRobot.head.onSale ? 'sale-border' : '';
