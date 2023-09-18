e76c3d306a4d2f41fa95c78e86dd35378c0b6618
yoniyoniee <shy7235@naver.com>
2023-09-18 13:28:25 +0900

    organizing modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex';
 // eslint-disable-next-line import/extensions
 import robotsModule from './modules/robots';
+import usersModule from './modules/users';
 
 export default createStore({
   modules: {
     robots: robotsModule,
+    users: usersModule,
   },
 });
